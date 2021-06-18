1da44f0fcbac63ac9adc8b7933f29381d88fbd52
Amjad Mustafa <amjadmus@buffalo.edu>
2021-06-18 15:29:42 -0400

    Added articles listing feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
   def show
-    
+
     @article = Article.find(params[:id]) #params gets what ever user inputted when calling a method.
+  end
+
+  def index
+    @articles = Article.all
+    
   end
 end
