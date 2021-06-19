a234907e2ae1747e9eae9dde3a5ebf251d228faf
Amjad Mustafa <amjadmus@buffalo.edu>
2021-06-18 23:27:27 -0400

    mapped APIS to CRUD actions and refactored code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy] #before_action helper runs the set_article function which was defined below in the givin actions
+
+
   def show
 
-    @article = Article.find(params[:id]) #params gets what ever user inputted when calling a method.
+
   end
 
   def index
@@ -14,11 +17,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfylly"
       redirect_to article_path(@article)
@@ -27,13 +30,27 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_params)
       flash[:notice] = "Article was updated!"
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+
+    @article.destroy
+    redirect_to articles_path
+
+  end
+  private
+  def set_article
+    return @article = Article.find(params[:id])  #params gets what ever user inputted when calling a method.
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
